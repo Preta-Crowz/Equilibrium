d76a7172054f63ff73c5909daea6213b97cbd3a6
Preta-Crowz <preta@siro.dev>
2022-12-28 17:49:41 +0900

    implement few upgrades

--- js/layers.js
+++ js/layers.js
@@ -7,19 +7,45 @@ addLayer("+", {
     startData() { return {
         unlocked: true,
         points: new Decimal(0),
+        best: new Decimal(0),
+        bestGain: new Decimal(0)
     }},
     color: "#CCCCCC",
     requires: new Decimal(2),
     resource: "Positive Energy",
+    midsection: [
+        [
+            "display-text",
+            function () { return `Your best PE Gain is ${player['+'].bestGain}` },
+            { "color": "#DFDFDF" }
+        ],
+        [
+            "display-text",
+            function() { return "Reset on this also resets every layers on negative side."},
+            { "color": "red", "font-size": "16px" }
+        ]
+    ],
+    baseResource: "Posibility",
     baseAmount() { return player.points },
+    update() {
+        tmp['+'].exponent = this.exponent;
+    },
     type: "normal",
-    exponent: 0.6,
+    get exponent() {
+        if (hasUpgrade('+', 14)) return 0.5;
+        return 0.4;
+    },
     gainMult() {
         mult = new Decimal(1);
         return mult;
     },
+    exptList: [
+        ['+', 13]
+    ],
     gainExp() {
-        return new Decimal(1);
+        let expt = new Decimal(1)
+
+        return expt;
     },
     hotkeys: [
         {key: "+", description: "+: Reset for Positive Points", onPress() {
@@ -29,14 +55,76 @@ addLayer("+", {
         }}
     ],
     layerShown() { return true },
-    onPrestige() { sideReset(0) },
+    onPrestige(n) {
+        n = new Decimal(n);
+        if (n.lt(1)) return;
+        if (n.gt(player[this.layer].bestGain)) player[this.layer].bestGain = n;
+        sideReset(0);
+    },
     canReset() { return !hasUpgrade('-', 11) },
+    parentMultList: [
+        ['+', 12]
+    ],
+    pointGen() {
+        if (player[this.layer].points.lte(new Decimal(0))) return new Decimal(0);
+        let logbase = new Decimal(4);
+        if (hasUpgrade('+', 21) && player[this.layer].best.gt(new Decimal(16))) logbase = Decimal.logarithm(logbase, player[this.layer].best).mul(8)
+        let base = Decimal.logarithm(player[this.layer].points.add(new Decimal(1)), logbase);
+        let minimal = new Decimal(0.1);
+        mult = new Decimal(1);
+        for (const u of this.parentMultList) {
+            if (hasUpgrade(...u)) mult = mult.mul(layers[u[0]].upgrades[u[1]].effect().positive.parent);
+        }
+        return Decimal.max(base, minimal).mul(mult);
+    },
     upgrades: {
         11: {
             title: "Positive Activation",
-            description: "Double \"Final\" Posibility Generation, Locks Negative",
-            cost: new Decimal(10),
-            effect() { return new Decimal(2) }
+            description: "Double \"Final\" Posibility Generation, Generate 0.5 Posibility per Second, Locks Negative",
+            cost: new Decimal(2),
+            effect() { return {
+                point: {
+                    gen: new Decimal(0.5),
+                    final: new Decimal(2)
+                }
+            }}
+        },
+        12: {
+            title: "Materialization",
+            description: "Posibility generation from PE will doubled",
+            cost: new Decimal(30),
+            effect() { return {
+                positive: {
+                    parent: new Decimal(2)
+                }
+            }}
+        },
+        13: {
+            title: "Replicate",
+            description: "Best gain of PE will power its gain",
+            effectDisplay() {
+                return '^' + (new Decimal(1).add(this.effect().positive.expt).toString())
+            },
+            cost: new Decimal(80),
+            effect() { return {
+                positive: {
+                    expt: player[this.layer].bestGain.div(new Decimal('1e2')).mul(3)
+                }
+            }}
+        },
+        14: {
+            title: "Better Processing",
+            description: "PE will generated on better formular<br>P^0.4 -> P^0.5",
+            cost: new Decimal(120)
+        },
+        21: {
+            title: "Reverse Revolution",
+            description: "Log base of P generation will based on best PE",
+            cost: new Decimal(200),
+            effectDisplay() {
+                if (player[this.layer].best.lte(16)) return 'n = 4'
+                return 'n = ' + Decimal.logarithm(4, player[this.layer].best).mul(8).toString();
+            }
         }
     }
 })
@@ -50,12 +138,39 @@ addLayer("-", {
     startData() { return {
         unlocked: true,
         points: new Decimal(0),
+        resetCount: new Decimal(0)
     }},
     color: "#777777",
     requires: new Decimal(2),
     resource: "Negative Energy",
+    midsection: [
+        [
+            "display-text",
+            function () { return `You resetted for NE ${player['-'].resetCount} time(s)` },
+            { "color": "#DFDFDF" }
+        ],
+        [
+            "display-text",
+            function() { return "Reset on this also resets every layers on Positive side."},
+            { "color": "red", "font-size": "16px" }
+        ]
+    ],
+    clickables: {
+        11: {
+            title: "Self Destruction",
+            unlocked() { return hasUpgrade('-', 14) },
+            canClick() { return player[this.layer].points.gt(0) },
+            onClick() {
+                player.points = new Decimal(0);
+                player[this.layer].points = new Decimal(0);
+                player[this.layer].resetCount = player[this.layer].resetCount.sub(0.4)
+                if (player[this.layer].resetCount.lt(0)) player[this.layer].resetCount = new Decimal(0);
+            }
+        },
+    },
+    baseResource: "Posibility",
     baseAmount() { return player.points },
-    type: "normal",
+    type: "static",
     exponent: 0.6,
     gainMult() {
         mult = new Decimal(1);
@@ -70,14 +185,66 @@ addLayer("-", {
         }}
     ],
     layerShown() { return true },
-    onPrestige() { sideReset(1) },
+    onPrestige(n) {
+        n = new Decimal(n);
+        if (n.lt(1)) return;
+        sideReset(1)
+        player[this.layer].resetCount = player[this.layer].resetCount.add(1);
+    },
     canReset() { return !hasUpgrade('+', 11) },
+    parentMultList: [
+        ['-', 12]
+    ],
+    pointGen() {
+        if (player[this.layer].points.lte(new Decimal(0))) return new Decimal(0);
+        let base = Decimal.logarithm(player[this.layer].points.add(new Decimal(1)), new Decimal(4));
+        let minimal = new Decimal(0.1);
+        mult = new Decimal(1);
+        for (const u of this.parentMultList) {
+            if (hasUpgrade(...u)) mult = mult.mul(layers[u[0]].upgrades[u[1]].effect().negative.parent);
+        }
+        return Decimal.max(base, minimal).mul(mult);
+    },
+    canBuyMax() { return hasUpgrade('-', 13) },
     upgrades: {
         11: {
             title: "Negative Activation",
-            description: "Double \"Final\" Posibility Generation, Locks Positive",
+            description: "Double \"Final\" Posibility Generation, Generate 0.5 Posibility per Second, Locks Positive",
+            cost: new Decimal(1),
+            effect() { return {
+                point: {
+                    gen: new Decimal(0.5),
+                    final: new Decimal(2)
+                }
+            }}
+        },
+        12: {
+            title: "Anti-materialization",
+            description: "Posibility generation from NE will doubled",
+            cost: new Decimal(5),
+            effect() { return {
+                negative: {
+                    parent: new Decimal(2)
+                }
+            }}
+        },
+        13: {
+            title: "Auto Recovery",
+            description: "Can buy max NE, Generate 1.2 P/s per NE Resets",
             cost: new Decimal(10),
-            effect() { return new Decimal(2) }
+            effectDisplay () {
+                return this.effect().point.gen + ' P/s';
+            },
+            effect() { return {
+                point: {
+                    gen: player['-'].resetCount.mul(1.2)
+                }
+            }}
+        },
+        14: {
+            title: "Self Destruction",
+            description: "Add self destruction to NE, reset current NE count with lose 0.4 reset.",
+            cost: new Decimal(30)
         }
     }
 })
--- js/mod.js
+++ js/mod.js
@@ -1,25 +1,25 @@
 let modInfo = {
-	name: "Equilibrium",
-	id: "eq.siro.dev",
-	author: "Koko Ayame",
-	pointsName: "Posibility",
-	modFiles: ["layers.js", "tree.js"],
+    name: "Equilibrium",
+    id: "eq.siro.dev",
+    author: "Koko Ayame",
+    pointsName: "Posibility",
+    modFiles: ["layers.js", "tree.js"],
 
-	discordName: "",
-	discordLink: "",
-	initialStartPoints: new Decimal (2), // Used for hard resets and new players
-	offlineLimit: 48,  // In hours
+    discordName: "",
+    discordLink: "",
+    initialStartPoints: new Decimal (2), // Used for hard resets and new players
+    offlineLimit: 48,  // In hours
 }
 
 // Set your version in num and name
 let VERSION = {
-	num: "0.0",
-	name: "Equilibrium",
+    num: "0.0",
+    name: "Equilibrium",
 }
 
 let changelog = `<h1>Changelog:</h1><br>
-	<h3>v0.0</h3><br>
-		- Added Positive & Negative`
+    <h3>v0.0</h3><br>
+        - Added Positive & Negative`
 
 let winText = `Congratulations! You have reached the end and beaten this game, but for now...`
 
@@ -33,32 +33,44 @@ function getStartPoints(){
 
 // Determines if it should show points/sec
 function canGenPoints(){
-	return true;
+    return true;
 }
 
+const pointGenList = [
+    ["+", 11],
+    ["-", 11],
+    ["-", 13]
+];
 // Calculate points/sec!
 function getPointGen() {
-	if(!canGenPoints())
-		return new Decimal(0)
+    if(!canGenPoints())
+        return new Decimal(0)
 
-	let gain = new Decimal(0);
-	gain = gain.add(player["+"].points);
-	gain = gain.add(player["-"].points);
+    let gain = new Decimal(0);
+    let mult = new Decimal(1);
+    gain = gain.add(layers["+"].pointGen());
+    gain = gain.add(layers["-"].pointGen());
 
-	return gain.mul(getPointFinalMult())
+    for (const u of pointGenList) {
+        if (!hasUpgrade(...u)) continue;
+        const eff = upgradeEffect(...u).point;
+        gain = gain.add(eff.gen);
+    }
+
+    return gain.mul(getPointFinalMult())
 }
 
 const finalMultList = [
-	["+", 11],
-	["-", 11]
+    ["+", 11],
+    ["-", 11]
 ];
 
 function getPointFinalMult() {
-	mult = new Decimal(1)
-	for (const u of finalMultList) {
-		if (hasUpgrade(...u)) mult = mult.mul(upgradeEffect(...u))
-	}
-	return mult;
+    mult = new Decimal(1)
+    for (const u of finalMultList) {
+        if (hasUpgrade(...u)) mult = mult.mul(upgradeEffect(...u).point.final);
+    }
+    return mult;
 }
 
 // You can add non-layer related variables that should to into "player" and be saved here, along with default values
@@ -71,7 +83,7 @@ var displayThings = [
 
 // Determines when the game "ends"
 function isEndgame() {
-	return player.points.gte(new Decimal("e280000000"))
+    return player.points.gte(new Decimal("e280000000"))
 }
 
 
@@ -85,7 +97,7 @@ var backgroundStyle = {
 
 // You can change this if you have things that can be messed up by long tick lengths
 function maxTickLength() {
-	return(3600) // Default is 1 hour which is just arbitrarily large
+    return(3600) // Default is 1 hour which is just arbitrarily large
 }
 
 // Use this if you need to undo inflation from an older version. If the version is older than the version that fixed the issue,
